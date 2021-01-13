37040fa6bd509474432f148c16059deac74539a1
Manasi <54620499+m4nasi@users.noreply.github.com>
2021-01-13 14:16:40 +0000

    Add files via upload

---
+++ Quiz/Quiz/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    class Program
+    {
+        static int score = 0;
+        static void AskQuestion(string question, string answer)
+        {
+            int attempts = 3;
+            string response1;
+
+            do
+            {
+                Console.WriteLine(question);
+                response1 = Console.ReadLine();
+
+                if (response1.ToLower() == answer) 
+                {
+                    Console.WriteLine("Well done!");
+                    score += attempts;
+                }
+
+                else
+                {
+                    Console.WriteLine("Sorry " + response1 + " is incorrect");
+                    if (--attempts != 0)
+                    {
+                        Console.WriteLine("you have " + attempts + " attempts remaining");
+                    }
+                }
+
+            } while (attempts > 0 && response1.ToLower() != answer.ToLower());
+        }
+        static void Main(string[] args)
+        {
+            AskQuestion("What is the capital of England?", "london");
+            AskQuestion("What is the capital of India?", "new delhi");
+            AskQuestion("What is the capital of Spain?", "madrid");
+            AskQuestion("What is the capital of France?", "paris");
+            AskQuestion("What is the capital of Japan?", "tokyo");
+
+            Console.WriteLine("The quiz is over, you scored " + score + " points");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Temperature Convertor/Temperature convertor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperature_convertor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter what fahrenheit you want to convert");
+            double celcius = Convert.ToDouble(Console.ReadLine());
+            double farhenheit = celcius * 9 / 5 + 32;
+            Console.WriteLine(celcius + " in celcius is " + farhenheit);
+            Console.ReadLine();
+
+            Console.WriteLine("What kilograms do you want to convert?");
+            double kilogram = Convert.ToDouble(Console.ReadLine());
+            double pound = kilogram * 2.205;
+            Console.WriteLine(kilogram +" in pounds is " + pound);
+            Console.ReadLine();
+        }   
+    }
+}
